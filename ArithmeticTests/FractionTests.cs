855d60b01a5debb4ddf1c7a3327378199403e675
MrLiauw <maryantoliauw@gmail.com>
2016-07-04 18:33:25 +0800

    make the test shorter

--- ArithmeticTests/FractionTests.cs
+++ ArithmeticTests/FractionTests.cs
@@ -7,36 +7,31 @@ namespace Arithmetic.Tests
         [TestMethod()]
         public void ZeroPlusZero()
         {
-            Fraction sum = new Fraction(0).plus(new Fraction(0));
-            Assert.AreEqual(0, sum.toIntegerValue());
+            Assert.AreEqual(0, new Fraction(0).plus(new Fraction(0)).toIntegerValue());
         }
 
         [TestMethod()]
         public void nonZeroPlusZero()
         {
-            Fraction sum = new Fraction(3).plus(new Fraction(0));
-            Assert.AreEqual(3, sum.toIntegerValue());
+            Assert.AreEqual(3, new Fraction(3).plus(new Fraction(0)).toIntegerValue());
         }
 
         [TestMethod()]
         public void ZeroPlusNonZero()
         {
-            Fraction sum = new Fraction(0).plus(new Fraction(3));
-            Assert.AreEqual(3, sum.toIntegerValue());
+            Assert.AreEqual(3, new Fraction(0).plus(new Fraction(3)).toIntegerValue());
         }
 
         [TestMethod()]
         public void nonNegativeNonZeroOperands()
         {
-            Fraction sum = new Fraction(3).plus(new Fraction(4));
-            Assert.AreEqual(7, sum.toIntegerValue());
+            Assert.AreEqual(7, new Fraction(3).plus(new Fraction(4)).toIntegerValue());
         }
 
         [TestMethod()]
         public void negativeInputAndNegativeOutput()
         {
-            Fraction sum = new Fraction(-3).plus(new Fraction(1));
-            Assert.AreEqual(-2, sum.toIntegerValue());
+            Assert.AreEqual(-2, new Fraction(-3).plus(new Fraction(1)).toIntegerValue());
         }
     }
 }
