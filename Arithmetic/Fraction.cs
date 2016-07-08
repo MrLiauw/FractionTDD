4eb9003a4b03ef28a796c958d40684a5a22c405f
MrLiauw <maryantoliauw@gmail.com>
2016-07-08 12:47:25 +0800

    remove need of integer value

--- Arithmetic/Fraction.cs
+++ Arithmetic/Fraction.cs
@@ -3,19 +3,16 @@
     public class Fraction
     {
         private readonly int _denominator;
-        private readonly int _integerValue;
         private readonly int _numerator;
 
         public Fraction(int integerValue)
         {
-            _integerValue = integerValue;
             _numerator = integerValue;
             _denominator = 1;
         }
 
         public Fraction(int numerator, int denominator)
         {
-            _integerValue = numerator;
             _numerator = numerator;
             _denominator = denominator;
         }
@@ -27,7 +24,7 @@
 
         public int toIntegerValue()
         {
-            return _integerValue;
+            return _numerator;
         }
 
         public int getNumerator {
