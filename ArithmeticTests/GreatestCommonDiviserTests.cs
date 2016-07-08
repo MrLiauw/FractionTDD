f7413df64958975327a820343efd8839e5b7a89e
MrLiauw <maryantoliauw@gmail.com>
2016-07-08 17:19:07 +0800

    explore gcd algorithm from wikipedia

--- ArithmeticTests/GreatestCommonDiviserTests.cs
+++ ArithmeticTests/GreatestCommonDiviserTests.cs
@@ -6,9 +6,41 @@ namespace ArithmeticTests
     public class GreatestCommonDiviserTests
     {
         [TestMethod]
-        public void oneAndOne()
+        public void reflexive()
         {
             Assert.AreEqual(1, gcd(1,1));
+            Assert.AreEqual(2, gcd(2, 2));
+            Assert.AreEqual(-1, gcd(-1, -1));
+        }
+
+        [TestMethod]
+        public void relativelyPrime()
+        {
+            Assert.AreEqual(1, gcd(2, 3));
+            Assert.AreEqual(1, gcd(4, 7));
+            Assert.AreEqual(-1, gcd(-2, -3));
+        }
+
+        [TestMethod]
+        public void oneIsMultipleOfTheOther()
+        {
+            Assert.AreEqual(3, gcd(3, 9));
+            Assert.AreEqual(5, gcd(5, 30));
+        }
+
+        [TestMethod]
+        public void commonFactor()
+        {
+            Assert.AreEqual(2, gcd(6, 8));
+            Assert.AreEqual(7, gcd(49, 315));
+            Assert.AreEqual(-4, gcd(-24, -28));
+        }
+
+        [TestMethod]
+        public void negatives()
+        {
+            Assert.AreEqual(4, gcd(-24, 28));
+            Assert.AreEqual(-4, gcd(24, -28));
         }
 
         private int gcd(int a, int b)
