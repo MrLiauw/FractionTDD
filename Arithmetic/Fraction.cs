80825d79536c414b1498a9c16bd11bdead9b0c78
MrLiauw <maryantoliauw@gmail.com>
2016-07-08 12:47:10 +0800

    no longer fake numerator

--- Arithmetic/Fraction.cs
+++ Arithmetic/Fraction.cs
@@ -4,22 +4,25 @@
     {
         private readonly int _denominator;
         private readonly int _integerValue;
+        private readonly int _numerator;
 
         public Fraction(int integerValue)
         {
             _integerValue = integerValue;
+            _numerator = integerValue;
             _denominator = 1;
         }
 
         public Fraction(int numerator, int denominator)
         {
             _integerValue = numerator;
+            _numerator = numerator;
             _denominator = denominator;
         }
 
         public Fraction plus(Fraction that)
         {
-            return new Fraction(_integerValue + that._integerValue, _denominator);
+            return new Fraction(_numerator + that._numerator, _denominator);
         }
 
         public int toIntegerValue()
@@ -28,7 +31,7 @@
         }
 
         public int getNumerator {
-            get { return 3; }
+            get { return _numerator; }
         }
 
         public int getDenominator {
